2b1916fd7c1af599a2778e8c1ade5d026f17093d
Joonhong Min <joonhong.min@epcommodities.cz>
2025-07-01 12:40:04 +0200

    add __all__

--- volue_insight_timeseries/__init__.py
+++ volue_insight_timeseries/__init__.py
@@ -7,6 +7,8 @@ import os
 from . import auth, curves, events, session, util
 from .session import Session
 
+__all__ = [ 'VERSION', 'Session', 'auth', 'curves', 'events', 'session', 'util', ]
+
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'VERSION')) as fv:
     VERSION = __version__ = fv.read().strip()
