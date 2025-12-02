d3fcea833fec8f4acf38d1554bfaab23a2a5ebd0
joonhong.min <joonhong.min@epcommodities.cz>
2025-12-02 14:23:05 +0100

    add async class, methods to __all__

--- volue_insight_timeseries/__init__.py
+++ volue_insight_timeseries/__init__.py
@@ -4,11 +4,11 @@
 
 import os
 
-from . import auth, curves, events, session, util
+from . import auth, curves, curves_async, events, session, session_async, util
 from .session import Session
+from .session_async import Asession
 
-__all__ = [ 'VERSION', 'Session', 'auth', 'curves', 'events', 'session', 'util', ]
-
+__all__ = ['VERSION', 'Asession', 'Session', 'auth', 'curves', 'curves_async', 'events', 'session', 'session_async', 'util']
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'VERSION')) as fv:
     VERSION = __version__ = fv.read().strip()
